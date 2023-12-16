121e413aa86ec54487e73f54e1e23eddce6e4f29
Manoj Kumar K <manojkumar9384@outlook.com>
2023-12-16 15:53:53 +0530

    Add files via upload

---
+++ get user input.py
@@ -0,0 +1,7 @@
+a=input("Enter Your Name ")
+a=str(a)
+b=input("enter your email ")
+b=str(b)
+c=input("enter the  phone number ")
+c=int(c)
+print("Name : "+a+"," "\nEmail : "+b+ "\nPhone number "+str(c))
