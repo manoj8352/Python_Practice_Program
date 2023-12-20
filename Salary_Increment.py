6ee91095c83d9000d05f82e7db729fa9e3c60df7
Manoj Kumar K <manojkumar9384@outlook.com>
2023-12-20 20:57:12 +0530

    Update Salary_Increment.py

--- Salary_Increment.py
+++ Salary_Increment.py
@@ -1,12 +1,13 @@
-"""Salary increment  according to the year that they have been worked in the company if the worked 1 year they are salary  will increase 1% """
+"""Salary increment , salary will increase according to their year of service ,if their year of service is less 5 year their not eligible for salary increment """
 
 #First getting the value form the user and store in the yearofservice variable
+print("enter you year of service in whole number format ")
 yearofservice=input("Enter the year of service : ")
 #using if condition to check the given value is equal to 5 years or more than 5 years
 def validinput():
-    if int(yearofservice) < 0 :
+    if int(yearofservice) < 5 :
      #if the yearofservice is less than 5 year 
-      print("Enter the valid number")
+      print("You are not eligible for salary increment ")
     elif int(yearofservice) > 5   or int(yearofservice) == 5:
      #if condition is true ,get the  salary detail  from the user and store the value in the salary variable  
          salary=int(input("Enter your salary : "))
@@ -18,15 +19,9 @@ def validinput():
 
     else:
        print("your not eligible for salary increment")
-
-
-
-
-
-
-
+#isdigit is used for checking where the input is only contain number
 if yearofservice.isdigit():
-     print("your are eligible for salary increment")
+     
      validinput()
 else:
     print("enter the valid number")     
